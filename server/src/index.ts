c23e6a9508f1b1ae1a2eda110ddb2278137e9da6
Kamlesh <138114923+xd-kamlesh@users.noreply.github.com>
2026-08-10 10:05:33 +0530

    Change PORT to be a number and update listen address

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
